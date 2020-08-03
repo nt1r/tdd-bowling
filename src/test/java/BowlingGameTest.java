85ab7c60d8eb9da8b22d23363860bb7d2c0669bd
leqi <michaelleqihust@gmail.com>
2020-08-03 17:12:43 +0800

    [leqi] feat: Finish strike tests without spare.

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BowlingGameTest {
     private final String allThrowZerosInputFilePath = "/Users/leqi/TWU_In-term/Week1-Day1/TDD_Fundamentals/tdd-bowling/src/test/java/data/all_throw_zeros.txt";
     private final String allThrowOnesInputFilePath = "/Users/leqi/TWU_In-term/Week1-Day1/TDD_Fundamentals/tdd-bowling/src/test/java/data/all_throw_ones.txt";
+    private final String strikeNotTheLastInputFilePath = "/Users/leqi/TWU_In-term/Week1-Day1/TDD_Fundamentals/tdd-bowling/src/test/java/data/strike_not_the_last.txt";
 
     BowlingGame bowlingGame;
     GameRule gameRule = new NormalGameRule();
@@ -45,6 +46,20 @@ public class BowlingGameTest {
         // When
         int actualScores = bowlingGame.calculateTotalLinesScore();
         int expectScores = 20;
+
+        // Then
+        assertEquals(actualScores, expectScores);
+    }
+
+    @Test
+    public void should_get_right_when_throw_contains_strike_not_contains_spare() {
+        // Given
+        List<List<Integer>> downPinsListStrikeNotTheLast = BowlInputFileReader.readDownPinsListFromFile(strikeNotTheLastInputFilePath);
+        bowlingGame = new BowlingGame(gameRule, downPinsListStrikeNotTheLast);
+
+        // When
+        int actualScores = bowlingGame.calculateTotalLinesScore();
+        int expectScores = 82;
 
         // Then
         assertEquals(actualScores, expectScores);
