90c5df92f0b81be11318504a6ea565d85beb0678
leqi <michaelleqihust@gmail.com>
2020-08-03 16:51:17 +0800

    [leqi] feat: Finish all one test.

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -7,7 +7,8 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
-    private final String allLineZeroInputFilePath = "/Users/leqi/TWU_In-term/Week1-Day1/TDD_Fundamentals/tdd-bowling/src/test/java/data/all_line_zero.txt";
+    private final String allThrowZerosInputFilePath = "/Users/leqi/TWU_In-term/Week1-Day1/TDD_Fundamentals/tdd-bowling/src/test/java/data/all_throw_zeros.txt";
+    private final String allThrowOnesInputFilePath = "/Users/leqi/TWU_In-term/Week1-Day1/TDD_Fundamentals/tdd-bowling/src/test/java/data/all_throw_ones.txt";
 
     BowlingGame bowlingGame;
     GameRule gameRule = new NormalGameRule();
@@ -23,12 +24,27 @@ public class BowlingGameTest {
     @Test
     public void should_get_zero_when_all_throws_are_zeros() {
         // Given
-        List<List<Integer>> downPinsListAllZeros = BowlInputFileReader.readDownPinsListFromFile(allLineZeroInputFilePath);
+        List<List<Integer>> downPinsListAllZeros = BowlInputFileReader.readDownPinsListFromFile(allThrowZerosInputFilePath);
         bowlingGame = new BowlingGame(gameRule, downPinsListAllZeros);
 
         // When
         int actualScores = bowlingGame.calculateTotalLinesScore();
         int expectScores = 0;
+
+        // Then
+        assertEquals(actualScores, expectScores);
+    }
+
+    @Test
+    public void should_get_twenty_when_all_throws_are_one() {
+        // Given
+        List<List<Integer>> downPinsListAllOnes = BowlInputFileReader.readDownPinsListFromFile(allThrowOnesInputFilePath);
+        // System.out.println(BowlInputFileReader.outputDebug(downPinsListAllOnes));
+        bowlingGame = new BowlingGame(gameRule, downPinsListAllOnes);
+
+        // When
+        int actualScores = bowlingGame.calculateTotalLinesScore();
+        int expectScores = 20;
 
         // Then
         assertEquals(actualScores, expectScores);
